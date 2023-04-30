e4f2c0c894906aabea32ea4ef591b59486ced78f
Ben <BenKalegin@users.noreply.github.com>
2023-04-30 16:28:35 -0400

    first version of 20 questions

---
+++ florida-dmv-exam-practice/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Quiz from './Quiz';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <Quiz />
+        </div>
+    );
+};
+
+export default App;
